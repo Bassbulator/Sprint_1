769c482d518d26d2c1514174de018ddd5658425a
Bassbulator <belikovm007@gmail.com>
2025-11-12 11:50:46 +0300

    Исправил замечание к заданию 2: убрал неиспользуемые атрибуты.

--- task_two.py
+++ task_two.py
@@ -1,8 +1,7 @@
 class Tester:
 
-    def __init__(self, name, deadline=True):
+    def __init__(self, name):
         self.name = name
-        self.deadline = deadline
 
     def work_hard(self, deadline=True):
         if deadline:
@@ -10,7 +9,7 @@ class Tester:
         else:
             print(self.name, 'Можно отдыхать')
 
-tester_1 = Tester(name='tester_1', deadline=True)
+tester_1 = Tester(name='tester_1')
 tester_1.work_hard(deadline=False)  # 'tester_1 Можно отдыхать'
 tester_2 = Tester(name='tester_2')
 tester_2.work_hard(deadline=True)   # 'tester_2 Что ж, ещё часок поработаю!'
