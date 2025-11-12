a3371a0973d942628042140ef5fd5d172c2afb55
Bassbulator <belikovm007@gmail.com>
2025-11-12 11:40:40 +0300

    Исправил замечание к заданию 1: Добавил обработку независимо от переданной строки.

--- task_one.py
+++ task_one.py
@@ -1,12 +1,27 @@
 string = '1h 45m,360s,25m,30m 120s,2h 60s'
 
-replaced_string = (string.replace('1h 45m', '105m')
-                   .replace('360s', '6m')
-                   .replace('30m 120s', '32m')
-                   .replace('2h 60s', '121m'))
+splited_string = string.split(',')
 
-minutes = replaced_string.split(',')
+total_minutes = 0
 
-total_minutes = [int(m[:-1]) for m in minutes]
+for min in splited_string:
+    min = min.replace(' ', '')
 
-print(sum(total_minutes))
+    if 'h' in min:
+        parts = min.split('h')
+        hours = int(parts[0])
+        total_minutes += hours * 60
+        min = parts[1]
+
+    if 'm' in min:
+        parts = min.split('m')
+        minutes = int(parts[0])
+        total_minutes += minutes
+        min = parts[1]
+
+    if 's' in min:
+        parts = min.split('s')
+        seconds = int(parts[0])
+        total_minutes += seconds // 60
+
+print(total_minutes)
